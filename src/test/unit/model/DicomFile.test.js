3fe046dd8e6bb184a3df555a68c14cd1161d2d38
Ronny Kursawe <r.kursawe@dkfz-heidelberg.de>
2022-02-15 14:04:28 +0100

    test: minimal test for CT Dicom file

--- src/test/unit/model/DicomFile.test.js
+++ src/test/unit/model/DicomFile.test.js
@@ -4,8 +4,9 @@
 import fs from 'fs';
 import path from 'path';
 import { promisify } from 'util';
+import DicomPropertyNames from '../../../constants/DicomPropertyNames';
+import Modalities from '../../../constants/Modalities';
 import DicomFile from '../../../model/DicomFile';
-import { replacer } from '../../util/ObjectToStringHelper';
 
 const readdir = promisify(fs.readdir)
 
@@ -18,12 +19,7 @@ describe('Test Dicom CT File parsing', () => {
 
     const file = new DicomFile();
     file.parseDicomData(dicomFileAsBuffer);
-    console.log(JSON.stringify(file, replacer));
-
-    test('StudyInstanceUID', () => {
-        expect(file.studyInstanceUID).toBeDefined();
-        expect(file.studyInstanceUID).toMatch(dicomUidRegex);
-    });
+    //    console.log(JSON.stringify(file, replacer));
 
     test('DicomFile object has all basic properties', () => {
         expect(file).toHasBasicDicomProperties(file);
@@ -33,24 +29,21 @@ describe('Test Dicom CT File parsing', () => {
 
 expect.extend({
     toHasBasicDicomProperties(file) {
-        const failMessages = [];
+        let failMessages = [];
 
-        const studyInstanceUID = file.studyInstanceUID;
+        failMessages = failMessages.concat(verifyDicomUidProperty('StudyInstanceUID', file.studyInstanceUID));
+        failMessages = failMessages.concat(verifyDicomUidProperty('SeriesInstanceUID', file.seriesInstanceUID));
 
-        if (studyInstanceUID == null) {
-            failMessages.push(`StudyInstanceUID is null or undefined.`)
-        } else {
-            if (!studyInstanceUID.match(dicomUidRegex)) {
-                failMessages.push(`StudyInstanceUID is not a Dicom UID.`)
-            }
-        }
+        failMessages = failMessages.concat(verifyModalityString(file.parsedParameters.get('Modality'), Modalities.CT));
+        failMessages = failMessages.concat(verifyCTProperties(file.parsedParameters));
 
+        failMessages = failMessages.concat(verifyDicomUidProperty('SeriesInstanceUID', file.seriesInstanceUID));
 
 
         if (failMessages.length === 0) {
             return {
                 message: () =>
-                    'Basic Dicom properties passed',
+                    'Basic Dicom properties test passed',
                 pass: true,
             };
         } else {
@@ -62,3 +55,78 @@ expect.extend({
         }
     },
 });
+
+function verifyDicomUidProperty(propertyName, propertyValue) {
+    const failMessages = [];
+    if (propertyValue == null) {
+        failMessages.push(`${propertyName} is null or undefined.`)
+    } else {
+        if (!propertyValue.match(dicomUidRegex)) {
+            failMessages.push(`${propertyName} is not a Dicom UID.`)
+        }
+    }
+
+    return failMessages;
+}
+
+function verifyModalityString(value, expectedModality) {
+    const failMessages = [];
+    if (value == null) {
+        failMessages.push(`Modality is null or undefined.`)
+    } else {
+        if (expectedModality != null) {
+            if (value != expectedModality)
+                failMessages.push(`${value} is not the expected modality ${expectedModality}.`)
+        } else {
+            if (!value in Modalities) {
+                failMessages.push(`${value} is not a modality.`)
+            }
+        }
+    }
+
+    return failMessages;
+}
+
+function verifyString(propertyName, propertyValue) {
+    const failMessages = [];
+    if (propertyValue == null) {
+        failMessages.push(`String \"${propertyName}\" is null or undefined.`)
+    } else {
+        if (propertyValue.length === 0) {
+            failMessages.push(`${propertyName} has no value.`);
+        }
+    }
+
+    return failMessages;
+}
+
+function verifyCTProperties(propertyMap) {
+    let failMessages = [];
+
+    const expectedProperties = [
+        DicomPropertyNames.SOPInstanceUID,
+        DicomPropertyNames.StudyInstanceUID,
+        DicomPropertyNames.SeriesInstanceUID,
+        DicomPropertyNames.FrameOfReferenceUID,
+        DicomPropertyNames.MediaStorageSOPInstanceUID,
+        DicomPropertyNames.BodyPartExamined,
+        DicomPropertyNames.StudyDescription,
+        DicomPropertyNames.SeriesDescription,
+        DicomPropertyNames.ImageType
+    ];
+
+    expectedProperties.forEach((value, index) => {
+        if (!propertyMap.has(value)) {
+            failMessages.push(`Expected CT property \"${value}\" not found. \"${index}\"`)
+        } else {
+            if (value.endsWith('UID')) {
+                failMessages = failMessages.concat(verifyDicomUidProperty(value, propertyMap.get(value)))
+            } else {
+                failMessages = failMessages.concat(verifyString(value, propertyMap.get(value)));
+            }
+        }
+
+    })
+
+    return failMessages;
+}
