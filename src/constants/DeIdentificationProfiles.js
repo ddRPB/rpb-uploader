12ffe659df82476c3ea811e252dc9058442cc01e
Ronny Kursawe <r.kursawe@dkfz-heidelberg.de>
2022-04-27 14:41:58 +0200

    chore: Adding Constants and Documentation for implementing DICOM DeIdentification

--- src/constants/DeIdentificationProfiles.js
+++ src/constants/DeIdentificationProfiles.js
@@ -1,6 +1,12 @@
+/**
+ *
+ * https://dicom.nema.org/medical/dicom/current/output/html/part15.html#table_E.1-1 
+ * 
+ */
+
 module.exports = Object.freeze({
     BASIC: 'Basic',
-    RETAIN_LONG_FULL_DATES: 'Retail Long Full Dates Option',
+    RETAIN_LONG_FULL_DATES: 'Retain Long Full Dates Option',
     RETAIN_PATIENT_CHARACTERISTICS: 'Retain Patient Characteristics Option',
     RETAIN_DEVICE_IDENTITY: 'Retain Device Identity Option',
     CLEAN_DESCRIPTORS: 'Clean Descriptors Option'
---
+++ src/constants/dicomTerminologyDefinitions/Readme.md
@@ -0,0 +1,7 @@
+# DICOM Controlled Terminology Definitions (Normative)
+
+The meaning of DICOM codes are defined in an annex. Constants related to that are defined in this folder.
+
+Link to the definitions:
+
+https://dicom.nema.org/medical/dicom/current/output/html/part16.html#DCM_113100
