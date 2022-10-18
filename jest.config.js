7cf2371df0c1fde76428844786516ae11c756d0a
Ronny Kursawe <r.kursawe@dkfz-heidelberg.de>
2022-10-18 14:54:03 +0200

    build: jest-expect-message adds messages to the expect statement that will allow to generate dynamic messages per expect statement, for instance: if the test runs in a for loop.

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,9 @@ Configuration for test environment JEST
  */
 
 module.exports = {
+  // adds custom messages to Jest expect
+  // https://github.com/mattphillips/jest-expect-message
+  "setupFilesAfterEnv": ["jest-expect-message"],
   // https://www.npmjs.com/package/identity-obj-proxy
   // *.css and *.less files will be mocked
   moduleNameMapper: {
