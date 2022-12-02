b9416cd7767342f377de97d1d554a498047708b6
Ronny Kursawe <r.kursawe@dkfz-heidelberg.de>
2022-12-02 10:37:44 +0100

    refactor: adapt text message if verification process failed

--- src/model/DicomUploadPackage.js
+++ src/model/DicomUploadPackage.js
@@ -434,7 +434,7 @@ export default class DicomUploadPackage {
 
                     data.error = e;
                     this.log.debug({ message: 'Query for specific series failed', data });
-                    errors.push({ message: 'Query for specific series failed', data });
+                    errors.push({ message: 'Query for series validation failed - please wait some seconds and push the retry button.', data });
                     return { errors: errors };
                 }
 
